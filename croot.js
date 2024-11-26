0e1cf5f8335824da663491c0eeba02e5b26e283d
Rolly Maulana Awangga <awangga@gmail.com>
2024-11-26 17:19:53 +0700

    masukin slider nya ke html

--- croot.js
+++ croot.js
@@ -1,6 +1,5 @@
-import {renderHTML,addJSInHead} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.7/element.js";
+import {renderHTML} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.7/element.js";
 
 
 
-await renderHTML("content","content/home.html");
-addJSInHead("slider.js");
+renderHTML("content","content/home.html");
