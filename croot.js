f6c6302e5491d531c6c0810162ba2c72ca7bae1c
Rolly Maulana Awangga <awangga@gmail.com>
2024-11-26 17:33:08 +0700

    tambah slider

--- croot.js
+++ croot.js
@@ -1,5 +1,13 @@
-import {renderHTML} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.7/element.js";
+import {renderHTML,addJSInHead} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.8/element.js";
 
 
 
-renderHTML("content","content/home.html");
+
+// Memanggil renderHTML dengan callback
+renderHTML('content', 'content/home.html', afterContentLoaded);
+
+function afterContentLoaded() {
+    console.log('Konten telah dimuat dan dimasukkan ke dalam DOM.');
+    addJSInHead("slider.js");
+    // Tambahkan kode tambahan di sini, misalnya inisialisasi event listener
+}
